399be0d7fb4a497c64f3e4dbcf57df6afff943e5
abrorilhuda <moh.abrorilhuda@gmail.com>
2025-03-10 16:25:17 +0700

    ad file fungsi

---
+++ fungsi.py
@@ -0,0 +1,46 @@
+# function
+def my_func():
+    print("my function")
+
+# memanggil func
+my_func()
+
+# function anda argument
+def cetak(data):
+    print(data)
+
+
+cetak("hello world")
+
+def Noarbitari(data1,data2,data3):
+    print(data1,data2,data3)
+
+Noarbitari("data1","data2","data3")
+# arbitari arguemnt
+def Arbitari(*args):
+    print(args)
+
+Arbitari("hello", "world","semua")
+
+
+# arbitari keyword
+def keywordData(**kwargs):
+    print(kwargs)
+
+keywordData(name="sulaimna",date="rabu-kamis")
+
+
+
+"""lambda func"""
+
+func = lambda a:a + 2
+
+
+#pangkat exemple
+
+def pangkat(n):
+    return lambda a:a ** n
+
+number = pangkat(3)
+
+print(number(3))
