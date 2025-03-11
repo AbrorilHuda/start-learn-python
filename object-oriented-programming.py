6fce918c56adbbe0da2525b4db1314cdfa6fc430
abrorilhuda <moh.abrorilhuda@gmail.com>
2025-03-11 17:39:51 +0700

    file dummy oop py

---
+++ object-oriented-programming.py
@@ -0,0 +1,12 @@
+class Testing:
+    def __init__(self,name,date):
+        self.name = name
+        self.date = date
+    def hello(self):
+        print("hello world")
+
+
+
+tes = Testing("name","date")
+
+tes.hello()
