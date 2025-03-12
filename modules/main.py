5431220157fe22c85d297165f380815d57e1d358
abrorilhuda <moh.abrorilhuda@gmail.com>
2025-03-12 16:45:51 +0700

    modules content in pyhon

---
+++ modules/main.py
@@ -0,0 +1,18 @@
+import platform
+import datetime
+import  math
+import mymodule as dc
+
+dc.cetak("hello world")
+dc.coverttotuple("data","lest","goo")
+
+
+namesytem = platform.system()
+print(namesytem)
+
+datenow = datetime.datetime.now()
+print(datenow)
+
+exp = math.sqrt(53)
+print(math.pi)
+print(exp)
---
+++ modules/mymodule.py
@@ -0,0 +1,5 @@
+def cetak(data):
+    print(data)
+
+def coverttotuple(*args):
+    print(args)
